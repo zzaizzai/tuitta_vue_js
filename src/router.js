f3092ffb2298542f622b2279c48c9ef1c185c9be
junsai window <junsai0530@gmail.com>
2022-08-21 23:15:47 +0900

    init

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from './components/Home.vue'
+import Profile from './components/Profile.vue'
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+import PostsView from './components/PostsView.vue'
+
+const routes = [
+    {
+        path: "/",
+        component: Home,
+    },
+    {
+        path: "/posts",
+        component: PostsView,
+    },
+
+    {
+        path: "/profile",
+        component: Profile,
+    },
+
+    {
+        path: "/register",
+        component: Register,
+    },
+    {
+        path: "/login",
+        component: Login,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+export default router; 
